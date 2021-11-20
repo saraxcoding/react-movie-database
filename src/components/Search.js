bd9197754ebe1afc353605fe2272612182634d63
sara_h <smoke_screen16@hotmail.com>
2021-11-19 23:38:24 -0500

    Added search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,9 @@ import React from "react";
 function Search () {
     return (
         <section className="searchbox-wrap">
-            
+            <input type="text" placeholder="Searvh for a movie..." className="searchnbox" />
         </section>
     )
 }
+
+export default Search
