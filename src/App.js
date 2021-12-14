c10805f9dbbf4bb8c34c84f7321b76d8c35c94c3
sara_h <smoke_screen16@hotmail.com>
2021-12-13 22:51:29 -0500

    Added more styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from axios;
+import axios from 'axios';
 
 import Search from "./components/Search";
 import Results from "./components/Results";
@@ -36,14 +36,14 @@ function App () {
       //console.log(state.s);
     }
 
-  return (
+  return ( 
     <div className="App">
       <header>
         <h1>Movie Database</h1>
       </header>
       <main>
         <Search handleInput={handleInput} search={search} />
-        <Results results={state.results} />
+        <Results results={state.results} /> 
       </main>
     </div>
   );
